aab21b6ea6024c05b66cf18faf1a492ee117aff1
tithi1234 <tithimodak1995@gmail.com>
2022-03-05 09:00:54 +0530

    counter add

--- src/component/functinal_component/Hooks/useState/HookCounter.js
+++ src/component/functinal_component/Hooks/useState/HookCounter.js
@@ -1,11 +1,20 @@
 import React,{useState} from 'react'
 
 function HookCounter() {
-    const [count,setCount]= useState(1)
+    const initialCount=0
+    const [count,setCount]= useState(initialCount)
+   const incrementFIve=()=>{
+     for(let i=0;i<5;i++){
+       setCount(preCount=>preCount+1)
+     }
+    }
   return (
     <div>
     <h1>Count:{count}</h1>
-    <button onClick={()=>setCount(count+1)} >Counter</button>
+    <button onClick={()=>setCount(count+1)} >Counter increment</button>
+    <button onClick={()=>setCount(count-1)} >Counter decrement</button>
+    <button onClick={()=>setCount(initialCount)} >Counter reset</button>
+    <button onClick={incrementFIve} >increment 5</button>
     </div>
   )
 }
